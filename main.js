0945be416aba825017f2c3fc8a057d3f26bad5fd
Felipe Mendes <felipemendesoliveira8@gmail.com>
2026-05-16 23:56:05 -0300

    Minha primeira requisição

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function main(){
-    console.log("Projeto inicial");
+const readlinePromises = require("node:readline/promises");
+const {stdin, stdout} = require("process"); //standardin e standardout = entrada e saída padrão (é o terminal de qualquer executável)
+
+async function main(){
+    const terminal = readlinePromises.createInterface(stdin, stdout);
+    
+    const nome = await terminal.question("Digite um nome de usuário do GitHub: ");
+    
+    //Node tem a função fetch que retorna uma response (resposta) em JSON
+    const response = await fetch(`https://api.github.com/users/${nome}`); //requisição
+    const json = await response.json();
+   
+    console.log(json);  //para ver o objeto completo
+    //console.log(json.login); //Para ver um dado específico do objeto
+    //console.log(json.html_url)
+    
+    terminal.close();
 }
 
 main();
